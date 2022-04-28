2bb61e0da8dbbd0e3d0a6713970c423a6ceb0f15
Mmx233 <admin@mmxblog.com>
2022-04-29 00:16:19 +0800

    feat: 尝试实现基于临时文件的数据缓存

---
+++ modules/cache/avatar.go
@@ -0,0 +1,59 @@
+package cache
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Mmx233/tool"
+	"os"
+	"path"
+	"time"
+)
+
+type AvatarCache struct {
+	URL     string
+	Expires int64
+}
+
+type avatar struct {
+	path func(uid uint) string
+}
+
+var Avatar = avatar{
+	path: func(uid uint) string {
+		return path.Join(os.TempDir(), "belive-avatar-"+fmt.Sprint(uid))
+	},
+}
+
+func (a *avatar) Cache(uid uint, url string, valid time.Duration) error {
+	d, _ := json.Marshal(&AvatarCache{
+		URL:     url,
+		Expires: time.Now().Add(valid).Unix(),
+	})
+	f, e := os.OpenFile(a.path(uid), os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0600)
+	if e != nil {
+		return e
+	}
+	defer f.Close()
+	_, e = f.Write(d)
+	return e
+}
+
+func (a *avatar) Read(uid uint) (string, error) {
+	if !tool.File.Exists(a.path(uid)) {
+		return "", Nil
+	}
+	f, e := os.OpenFile(a.path(uid), os.O_RDONLY, 0600)
+	if e != nil {
+		return "", e
+	}
+	defer f.Close()
+	var data AvatarCache
+	e = json.NewDecoder(f).Decode(&data)
+	if e != nil {
+		return "", e
+	}
+	if data.Expires < time.Now().Unix() {
+		return "", Nil
+	}
+	return data.URL, nil
+}
---
+++ modules/cache/base.go
@@ -0,0 +1,5 @@
+package cache
+
+import "errors"
+
+var Nil = errors.New("cache: nil")
